8870e23b95cb4433cee4a21a2b46fc0c6d33203f
wawatai <100764746+wawatai@users.noreply.github.com>
2023-11-16 10:13:35 +0800

    112-1116-1-J
    ADD ANIMATE && EDIT ABSOLUT POSITION

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,33 +1,33 @@
-// var numberOfEls = 1000;
-// var duration = 8000;
-// var midScreenX = window.innerWidth / 2;
-// var midScreenY = window.innerHeight / 2;
-// var radius = Math.sqrt(midScreenX * midScreenX + midScreenY * midScreenY);
-// var fragment = document.createDocumentFragment();
+var numberOfEls = 1000;
+var duration = 8000;
+var midScreenX = window.innerWidth / 2;
+var midScreenY = window.innerHeight / 2;
+var radius = Math.sqrt(midScreenX * midScreenX + midScreenY * midScreenY);
+var fragment = document.createDocumentFragment();
 
-// for (var i = 0; i < numberOfEls; i++) {
-//   var hue = Math.round(360 / numberOfEls * i);
-//   var angle = Math.random() * Math.PI * 2;
-//   var el = document.createElement('div');
-//   el.classList.add('particule');
-//   el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)';
-//   el.style.width = '1px';
-//   el.style.height = '1px';
-//   anime({
-//     targets: el,
-//     width: ['1px', '10px'],
-//     height: ['1px', '10px'],
-//     left: [midScreenX + 'px', Math.cos(angle) * radius + midScreenX + 'px'],
-//     top: [midScreenY + 'px', Math.sin(angle) * radius + midScreenY + 'px'],
-//     delay: (duration / numberOfEls) * i,
-//     duration: duration,
-//     easing: 'easeInExpo',
-//     loop: true
-//   });
-//   fragment.appendChild(el);
-// }
+for (var i = 0; i < numberOfEls; i++) {
+  var hue = Math.round(360 / numberOfEls * i);
+  var angle = Math.random() * Math.PI * 2;
+  var el = document.createElement('div');
+  el.classList.add('particule');
+  el.style.backgroundColor = 'hsl(' + hue + ', 40%, 60%)';
+  el.style.width = '1px';
+  el.style.height = '1px';
+  anime({
+    targets: el,
+    width: ['1px', '10px'],
+    height: ['1px', '10px'],
+    left: [midScreenX + 'px', Math.cos(angle) * radius + midScreenX + 'px'],
+    top: [midScreenY + 'px', Math.sin(angle) * radius + midScreenY + 'px'],
+    delay: (duration / numberOfEls) * i,
+    duration: duration,
+    easing: 'easeInExpo',
+    loop: true
+  });
+  fragment.appendChild(el);
+}
 
-// document.body.appendChild(fragment);
+document.body.appendChild(fragment);
 
 
 (() => {
